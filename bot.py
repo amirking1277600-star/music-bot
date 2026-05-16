be80e250f02323b7345971728ca392aec647d438
amirking1277600-star <amirking1277600@gmail.com>
2026-05-16 15:58:54 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import yt_dlp
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'البوت شغال وبقى أونلاين باسم: {bot.user}')
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("لازم تدخل روم صوتي الأول يا صاحبي!")
+
+@bot.command()
+async def play(ctx, *, url):
+    if not ctx.voice_client:
+        if ctx.author.voice:
+            await ctx.author.voice.channel.connect()
+        else:
+            return await ctx.send("ادخل روم صوتي الأول!")
+            
+    ctx.voice_client.stop()
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    
+    with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(f"ytsearch:{url}", download=False)['entries'][0]
+        url2 = info['url']
+        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+        ctx.voice_client.play(source)
+    await ctx.send(f"شغال دلوقتي: {info['title']}")
+
+@bot.command()
+async def stop(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+
+bot.run('MTUwNTE2MDc3Mzg3MjM5MDE3NA.G0k9DG.NO0UKO7ikASQODMzR3avjbTOMxaJdqXpblNxdE')
